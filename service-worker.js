2e6ade784230343eccf5ca271183f847cc372940
destcfw-bit <destcfw@gmail.com>
2026-09-13 05:12:38 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tahweeshti-static-v13';
+const CACHE='tahweeshti-static-v14';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.json','./assets/logo.png','./assets/icon-192.png','./assets/icon-512.png','./assets/favicon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
